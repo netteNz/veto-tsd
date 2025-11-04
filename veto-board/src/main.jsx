d9c8eb5d247bb3bea95080e952b7698c8a3a6eff
Emanuel Lugo <lugo.emanuel@gmail.com>
2025-11-03 22:26:24 -0500

    Initialized analytics

--- veto-board/src/main.jsx
+++ veto-board/src/main.jsx
@@ -2,7 +2,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import analytics from './lib/analytics.js'
 import './index.css'
+
+// Initialize GA4 before rendering
+analytics.init({ 
+  measurementId: 'G-5Z9VYJD8D5',
+  autoTrack: true 
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
